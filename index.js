9708288e5449c7c2e6535194e5c1789e572af301
Linus Kipkoech <kipkoechlinuss@berkeley.edu>
2018-05-24 12:31:46 -0700

    changed index.js

--- index.js
+++ index.js
@@ -2,7 +2,36 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there by buddyyy' });
+  res.send({ <?php
+	$title = 'Statewide Database | Updates';
+	$mainCSS = 1;
+	$tableCSS = 0;
+	require_once('include/linkPath_final.php');
+	require_once($linkToHeader);
+	require_once("updates.php");
+?>
+
+<!-- To add a new update, go to updates.php! -->
+
+<div id='content'>
+	<h1>Latest Updates</h1>
+	<div class="row body carousel-container">
+		<div class="col">
+	 		<div class="row carousel-body">
+	 			<div class="carousel-content">
+	 				<?php
+	 					foreach ($updates as $value) {
+	 						echo $value;
+	 					}
+	 				?>
+		 		</div>
+	 		</div>
+	 	</div>
+	</div>
+</div>
+
+<?php require_once($linkToFooter); ?>
+ });
 });
 //dynamic port binding. Tells the server if we don't have another port idr 5000
 var PORT_NUMBER = process.env.PORT || 5000;
