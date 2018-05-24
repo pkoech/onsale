efc030f6248ad3ca9641be0df7c3539cabc2693a
Linus Kipkoech <kipkoechlinuss@berkeley.edu>
2018-05-24 12:27:39 -0700

    changed index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'there by buddyyy' });
 });
 //dynamic port binding. Tells the server if we don't have another port idr 5000
 var PORT_NUMBER = process.env.PORT || 5000;
